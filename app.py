a1dbae697c3536f091d221a3613d3862acaf5f02
Bahrom <bahrom77774444@gmail.com>
2022-09-14 20:40:59 +0500

    project files have been uploaded

---
+++ app.py
@@ -0,0 +1,20 @@
+import posixpath
+import streamlit as st
+import pathlib
+temp=pathlib.posixpath
+pathlib.PosixPath=pathlib.WindowsPath
+import plotly.express as px
+from fastai.vision.all import *
+
+st.title('Transportlarni klassifikatsiyalovchi model')
+
+file=st.file_uploader('Rasm yuklash',type=['jpeg','jpg','svg'])
+if file:
+    st.image(file)
+    img=PILImage.create(file)
+    model=load_learner('transport_model.pkl')
+    pred,pred_id,probs=model.predict(img)
+    st.success(f"Bashorat:{pred}")
+    st.info(f"Ehtimollik:{probs[pred_id]*100:1f}%")
+    fig=px.bar(x=probs*100,y=model.dls.vocab)
+    st.plotly_chart(fig)
